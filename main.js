e37761597af83a99df8f723c2fa0185fa9c1b38f
CamCoder <camcam.95@icloud.com>
2021-07-08 19:24:02 -0500

    Made side menu responsive

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ let navbar = document.querySelector("nav");
 let menu = document.querySelector(".navigation");
 let main = document.querySelector(".main")
 
+let navClose = document.querySelector(".nav-close")
+
 toggle.onclick = function() {
+    toggle.classList.toggle('active');
+    navbar.classList.toggle('active');
+    menu.classList.toggle('active');
+    main.classList.toggle('active');
+}
+
+navClose.onclick = function() {
     toggle.classList.toggle('active');
     navbar.classList.toggle('active');
     menu.classList.toggle('active');
